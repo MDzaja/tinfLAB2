1cfc591e02208a633716d5e8ff1e2b87a44703f2
MatejC <33391902+MCubek@users.noreply.github.com>
2020-01-08 21:04:55 +0100

    Added constructor, basic getter and setter and first problem implemented.

--- src/hr/fer/tinf/lab/group0503/BinaryBlockCode.java
+++ src/hr/fer/tinf/lab/group0503/BinaryBlockCode.java
@@ -1,9 +1,78 @@
 package hr.fer.tinf.lab.group0503;
 
-public class BinaryBlockCode {
-    //TODO
+import java.util.Arrays;
 
-    public static void main(String[] args){
+public class BinaryBlockCode {
+    private boolean[][] gMatrix;
+
+    /**
+     * Constructor with boolean matrix
+     *
+     * @param gMatrix boolean gMatrix
+     */
+    public BinaryBlockCode(boolean[][] gMatrix) {
+        this.gMatrix = gMatrix;
+    }
+
+    /**
+     * Constructor with int matrix
+     *
+     * @param gMatrix int matrix
+     */
+    public BinaryBlockCode(int[][] gMatrix) {
+        int columns = gMatrix[0].length;
+        int rows = gMatrix.length;
+        this.gMatrix = new boolean[columns][rows];
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                this.gMatrix[i][j] = gMatrix[i][j] == 1;
+            }
+        }
+    }
+
+    public boolean[][] getgMatrix() {
+        return gMatrix;
+    }
+
+    public void setgMatrix(boolean[][] gMatrix) {
+        this.gMatrix = gMatrix;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BinaryBlockCode that = (BinaryBlockCode) o;
+
+        return Arrays.deepEquals(gMatrix, that.gMatrix);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(gMatrix);
+    }
+
+    /**
+     * Get num of columns
+     *
+     * @return number of columns
+     */
+    public int getN() {
+        return gMatrix[0].length;
+    }
+
+    /**
+     * Get number of rows
+     *
+     * @return number of rows
+     */
+    public int getK() {
+        return gMatrix.length;
+    }
+
+
+    public static void main(String[] args) {
         //TODO
     }
 }
